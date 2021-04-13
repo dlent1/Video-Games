44a69a10c3d09922783a407a4fe1ec7ece64ff53
dlent1 <dave_meade@yahoo.com>
2021-04-13 17:03:47 -0500

    dasfd

---
+++ RemoveTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RemoveTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worl d!");
+        }
+    }
+}
